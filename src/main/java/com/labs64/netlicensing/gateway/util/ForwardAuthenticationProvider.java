f413a03dd6daf2e5050c5c115cc4cfcc47c5f147
ayushkevich <yushkevich@me.com>
2019-10-14 15:24:31 +0300

    fix security auth #11

--- src/main/java/com/labs64/netlicensing/gateway/util/ForwardAuthenticationProvider.java
+++ src/main/java/com/labs64/netlicensing/gateway/util/ForwardAuthenticationProvider.java
@@ -6,20 +6,18 @@ import java.util.List;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 public class ForwardAuthenticationProvider implements AuthenticationProvider {
 
     @Override
-    public Authentication authenticate(final Authentication authentication) throws AuthenticationException {
+    public Authentication authenticate(final Authentication authentication) {
         final String name = authentication.getName();
         final String password = authentication.getCredentials().toString();
         final List<GrantedAuthority> grantedAuths = new ArrayList<>();
         grantedAuths.add(new SimpleGrantedAuthority("ROLE_VENDOR"));
-        final Authentication auth = new UsernamePasswordAuthenticationToken(name, password, grantedAuths);
-        return auth;
+        return new UsernamePasswordAuthenticationToken(name, password, grantedAuths);
     }
 
     @Override
